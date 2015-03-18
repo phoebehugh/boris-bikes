cb348ed366ffa7f060a3832443d59629be5d736d
Phoebe Hugh <phoebehugh@gmail.com>
2015-03-18 08:33:30 +0000

    Refactored code to make it more efficient

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,7 +1,6 @@
 require 'bike'
 
 describe Bike do
-  it { is_expected.to respond_to :broken? }
   context 'when created' do
     it { is_expected.not_to be_broken }
   end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,8 @@
 require 'docking_station'
 
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
+  it 'releases bikes that are not broken' do
+    bike = subject.release_bike
+    expect(bike).not_to be_broken
+  end
 end
